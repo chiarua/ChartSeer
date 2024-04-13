f779b31f75024add04ffd6f366022c23622d1872
chiarua <2721783259@qq.com>
2024-04-13 18:51:24 +0800

    reconstruct initialize

--- interface/modelserver.py
+++ interface/modelserver.py
@@ -34,6 +34,42 @@ m = re.search(r'_L(\d+)_', modelsave)
 # MAX_LEN = 20   #潜在空间的维度20
 MAX_LEN = 20
 LATENT = int(m.group(1))
+INST = "我想要分析这个车辆数据集，参考以下只有设计属性的可视化图表，给出多种完整的可视化图表，并进行解释 只有设计属性的可视化图表："
+DATA = '''数据集： "attributes": [["Miles_per_Gallon", "num", "quantitative"], ["Cylinders", "num", "ordinal"], 
+["Displacement", "num", "quantitative"], ["Horsepower", "num", "quantitative"], ["Weight_in_lbs", "num", 
+"quantitative"],["Acceleration", "num", "quantitative"], ["Year", "str", "temporal"], ["Origin", "str", "nominal"]], 
+"data": [ { "Name":"chevrolet chevelle malibu", "Miles_per_Gallon":18, "Cylinders":8, "Displacement":307, 
+"Horsepower":130, "Weight_in_lbs":3504, "Acceleration":12, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"buick 
+skylark 320", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":350, "Horsepower":165, "Weight_in_lbs":3693, 
+"Acceleration":11.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth satellite", "Miles_per_Gallon":18, 
+"Cylinders":8, "Displacement":318, "Horsepower":150, "Weight_in_lbs":3436, "Acceleration":11, "Year":"1970-01-01", 
+"Origin":"USA" }, { "Name":"amc rebel sst", "Miles_per_Gallon":16, "Cylinders":8, "Displacement":304, 
+"Horsepower":150, "Weight_in_lbs":3433, "Acceleration":12, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford 
+torino", "Miles_per_Gallon":17, "Cylinders":8, "Displacement":302, "Horsepower":140, "Weight_in_lbs":3449, 
+"Acceleration":10.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford galaxie 500", "Miles_per_Gallon":15, 
+"Cylinders":8, "Displacement":429, "Horsepower":198, "Weight_in_lbs":4341, "Acceleration":10, "Year":"1970-01-01", 
+"Origin":"USA" }, { "Name":"chevrolet impala", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":454, 
+"Horsepower":220, "Weight_in_lbs":4354, "Acceleration":9, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth 
+fury iii", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":440, "Horsepower":215, "Weight_in_lbs":4312, 
+"Acceleration":8.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"pontiac catalina", "Miles_per_Gallon":14, 
+"Cylinders":8, "Displacement":455, "Horsepower":225, "Weight_in_lbs":4425, "Acceleration":10, "Year":"1970-01-01", 
+"Origin":"USA" }, { "Name":"amc ambassador dpl", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":390, 
+"Horsepower":190, "Weight_in_lbs":3850, "Acceleration":8.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"citroen 
+ds-21 pallas", "Miles_per_Gallon":null, "Cylinders":4, "Displacement":133, "Horsepower":115, "Weight_in_lbs":3090, 
+"Acceleration":17.5, "Year":"1970-01-01", "Origin":"Europe" }, { "Name":"chevrolet chevelle concours (sw)", 
+"Miles_per_Gallon":null, "Cylinders":8, "Displacement":350, "Horsepower":165, "Weight_in_lbs":4142, 
+"Acceleration":11.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford torino (sw)", "Miles_per_Gallon":null, 
+"Cylinders":8, "Displacement":351, "Horsepower":153, "Weight_in_lbs":4034, "Acceleration":11, "Year":"1970-01-01", 
+"Origin":"USA" }, { "Name":"plymouth satellite (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":383, 
+"Horsepower":175, "Weight_in_lbs":4166, "Acceleration":10.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"amc 
+rebel sst (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":360, "Horsepower":175, "Weight_in_lbs":3850, 
+"Acceleration":11, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"dodge challenger se", "Miles_per_Gallon":15, 
+"Cylinders":8, "Displacement":383, "Horsepower":170, "Weight_in_lbs":3563, "Acceleration":10, "Year":"1970-01-01", 
+"Origin":"USA" }, { "Name":"plymouth 'cuda 340", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":340, 
+"Horsepower":160, "Weight_in_lbs":3609, "Acceleration":8, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford 
+mustang boss 302", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":302, "Horsepower":140, 
+"Weight_in_lbs":3353, "Acceleration":8, "Year":"1970-01-01", "Origin":"USA" } ]'''
+
 
 # rules = []
 # with open(rulesfile, 'r') as inputs:
@@ -102,9 +138,9 @@ def decode():
 @app.route('/decode_llm', methods=['POST'])
 def decode_llm():
     vlg = VegaLiteGenerator()
-    str = "我想要分析这个车辆数据集，参考以下只有设计属性的可视化图表，给出多种完整的可视化图表，并进行解释 只有设计属性的可视化图表："
+    str = INST
     vegas = request.get_json()[0]
-    data = '''数据集： "attributes": [["Miles_per_Gallon", "num", "quantitative"], ["Cylinders", "num", "ordinal"], ["Displacement", "num", "quantitative"], ["Horsepower", "num", "quantitative"], ["Weight_in_lbs", "num", "quantitative"],["Acceleration", "num", "quantitative"], ["Year", "str", "temporal"], ["Origin", "str", "nominal"]], "data": [ { "Name":"chevrolet chevelle malibu", "Miles_per_Gallon":18, "Cylinders":8, "Displacement":307, "Horsepower":130, "Weight_in_lbs":3504, "Acceleration":12, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"buick skylark 320", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":350, "Horsepower":165, "Weight_in_lbs":3693, "Acceleration":11.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth satellite", "Miles_per_Gallon":18, "Cylinders":8, "Displacement":318, "Horsepower":150, "Weight_in_lbs":3436, "Acceleration":11, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"amc rebel sst", "Miles_per_Gallon":16, "Cylinders":8, "Displacement":304, "Horsepower":150, "Weight_in_lbs":3433, "Acceleration":12, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford torino", "Miles_per_Gallon":17, "Cylinders":8, "Displacement":302, "Horsepower":140, "Weight_in_lbs":3449, "Acceleration":10.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford galaxie 500", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":429, "Horsepower":198, "Weight_in_lbs":4341, "Acceleration":10, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"chevrolet impala", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":454, "Horsepower":220, "Weight_in_lbs":4354, "Acceleration":9, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth fury iii", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":440, "Horsepower":215, "Weight_in_lbs":4312, "Acceleration":8.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"pontiac catalina", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":455, "Horsepower":225, "Weight_in_lbs":4425, "Acceleration":10, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"amc ambassador dpl", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":390, "Horsepower":190, "Weight_in_lbs":3850, "Acceleration":8.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"citroen ds-21 pallas", "Miles_per_Gallon":null, "Cylinders":4, "Displacement":133, "Horsepower":115, "Weight_in_lbs":3090, "Acceleration":17.5, "Year":"1970-01-01", "Origin":"Europe" }, { "Name":"chevrolet chevelle concours (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":350, "Horsepower":165, "Weight_in_lbs":4142, "Acceleration":11.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford torino (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":351, "Horsepower":153, "Weight_in_lbs":4034, "Acceleration":11, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth satellite (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":383, "Horsepower":175, "Weight_in_lbs":4166, "Acceleration":10.5, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"amc rebel sst (sw)", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":360, "Horsepower":175, "Weight_in_lbs":3850, "Acceleration":11, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"dodge challenger se", "Miles_per_Gallon":15, "Cylinders":8, "Displacement":383, "Horsepower":170, "Weight_in_lbs":3563, "Acceleration":10, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"plymouth 'cuda 340", "Miles_per_Gallon":14, "Cylinders":8, "Displacement":340, "Horsepower":160, "Weight_in_lbs":3609, "Acceleration":8, "Year":"1970-01-01", "Origin":"USA" }, { "Name":"ford mustang boss 302", "Miles_per_Gallon":null, "Cylinders":8, "Displacement":302, "Horsepower":140, "Weight_in_lbs":3353, "Acceleration":8, "Year":"1970-01-01", "Origin":"USA" } ]'''
+    data = DATA
     vlg.generate(str+vegas+data)
     codes = vlg.get_codes()
     expls = vlg.get_explanation()
